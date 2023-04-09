b68837c63b32ac9be6d0da06c8016bbdbced6c11
Ben Brophy <bdbrophy@gmail.com>
2023-04-09 21:48:06 +0000

    Add application form that stores applications in database

--- database.py
+++ database.py
@@ -30,3 +30,8 @@ def load_job_from_db(id):
       return None
     else:
       return dict(zip(column_names, rows[0]))
+
+def add_application_to_db(job_id, data):
+  with engine.connect() as conn:
+    query = text("INSERT INTO applications (job_id, full_name, email, linkedin_url, education, work_experience, resume_url) VALUES (:job_id, :full_name, :email, :linkedin_url, :education, :work_experience, :resume_url)")
+    conn.execute(query, {'job_id': job_id, 'full_name': data['full_name'], 'email': data['email'], 'education': data['education'], 'work_experience': data['work_experience'], 'resume_url': data['resume_url'], 'linkedin_url': data['linkedin_url']})
